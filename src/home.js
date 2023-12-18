92cfc1e845d27eaf67185207c0b8a0eae71770bd
PsammystPrime <samuelprime384@gmail.com>
2023-12-18 12:59:54 +0300

    Indent tags

--- src/home.js
+++ src/home.js
@@ -1,8 +1,9 @@
-const body = document.querySelector('.body')
-import Icon from './drinksbg2.jpeg'
+const body = document.querySelector('.body');
+import Icon from './drinksbg2.jpeg';
 
 const image = document.createElement('img')
 const about = document.createElement('div');
+const title = document.createElement('h1');
 const aboutheader = document.createElement('h5');
 const aboutPara = document.createElement('p');
 const aboutBtn = document.createElement('button');
@@ -12,6 +13,7 @@ image.alt = 'page'
 image.src = Icon
 image.id = 'mainImg'
 about.className = 'about'
+title.textContent = 'STAFOODS RESTAURANT'
 aboutheader.textContent = 'About us'
 aboutPara.textContent = " This is a very good restaurant. A page for dishes served in a restaurant. Practicing DOM manipulation by dynamically  rendering a simple restaurant homepage. Am using Javascript alone to generate the entire contents of the website!"
 aboutBtn.textContent = 'Loyalty Programme'
@@ -19,18 +21,17 @@ aboutBtn.className = 'loyalty'
 
 aboutBtn.addEventListener('click', ()=>{
     alert('Loyalty Programme Coming Soon')
-})
-
-
+});
 
 function home() {
-    body.textContent = ''
+    body.textContent = '';
 
+    about.appendChild(title);
     about.appendChild(aboutheader);
     about.appendChild(aboutPara);
     about.appendChild(aboutBtn);
     body.appendChild(image);
     body.appendChild(about);
 }
-export {home}
+export {home};
 
--- src/pageLoad.js
+++ src/pageLoad.js
@@ -1,23 +1,22 @@
-console.log('hey');
 const content = document.getElementById('content');
-import Icon from './drinksbg2.jpeg'
+import Icon from './drinksbg2.jpeg';
 
 const header = document.createElement('div');
 const homeBtn = document.createElement('button');
 const mealsBtn = document.createElement('button');
 const drinksBtn = document.createElement('button');
 
-header.className = 'header'
-homeBtn.className = 'home'
+header.className = 'header';
+homeBtn.className = 'home';
 homeBtn.textContent = 'Home';
-mealsBtn.textContent = 'Meals'
+mealsBtn.textContent = 'Meals';
 mealsBtn.className = 'meals';
 drinksBtn.textContent = 'Drinks';
-drinksBtn.className = 'drinks'
+drinksBtn.className = 'drinks';
 
 header.appendChild(homeBtn);
 header.appendChild(mealsBtn);
-header.appendChild(drinksBtn)
+header.appendChild(drinksBtn);
 content.appendChild(header);
 
 
@@ -29,17 +28,17 @@ const aboutheader = document.createElement('h5');
 const aboutPara = document.createElement('p');
 const aboutBtn = document.createElement('button');
 
-body.className = 'body'
-image.alt = 'page'
+body.className = 'body';
+image.alt = 'page';
 image.src = Icon;
-image.id = 'mainImg'
-about.className = 'about'
-title.textContent = 'STAFOODS RESTAURANT'
-aboutheader.textContent = 'About us'
+image.id = 'mainImg';
+about.className = 'about';
+title.textContent = 'STAFOODS RESTAURANT';
+aboutheader.textContent = 'About us';
 aboutPara.textContent = " This is a very good restaurant. A page for dishes served in a restaurant. Practicing DOM manipulation by dynamically  rendering a simple restaurant homepage. Am using Javascript alone to generate the entire contents of the website!"
 
-aboutBtn.className = 'loyalty'
-aboutBtn.textContent = 'Loyalty Programme'
+aboutBtn.className = 'loyalty';
+aboutBtn.textContent = 'Loyalty Programme';
 
 about.appendChild(title);
 about.appendChild(aboutheader);
@@ -61,9 +60,9 @@ const facebookBtn = document.createElement('button');
 const emailBtn = document.createElement('button');
 const githubBtn = document.createElement('button');
 
-left.className = 'left'
-right.className = 'right'
-right.textContent = `Images generated from Unsplash`
+left.className = 'left';
+right.className = 'right';
+right.textContent = `Images generated from Unsplash`;
 footerHeader.textContent = 'Connect on';
 connectBtns.className = ' Connect';
 twitterBtn.textContent = 'Twitter';
@@ -72,14 +71,14 @@ facebookBtn.textContent = 'Facebook';
 emailBtn.textContent = 'Email';
 githubBtn.textContent = 'Github';
 
-connectBtns.appendChild(twitterBtn)
+connectBtns.appendChild(twitterBtn);
 connectBtns.appendChild(instagramBtn);
 connectBtns.appendChild(facebookBtn);
 connectBtns.appendChild(emailBtn);
 connectBtns.appendChild(githubBtn);
 left.appendChild(footerHeader);
 left.appendChild(connectBtns);
-footer.appendChild(left)
-footer.appendChild(right)
+footer.appendChild(left);
+footer.appendChild(right);
 content.appendChild(footer);
 
